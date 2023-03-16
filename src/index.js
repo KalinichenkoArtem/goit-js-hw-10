fb113d0ed356d13270179f0ffbc0496e1c3082a9
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-03-16 17:30:50 +0200

    imp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import debounce from 'lodash.debounce';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+import { fetchCountries } from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
